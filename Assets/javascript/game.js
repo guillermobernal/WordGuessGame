bf67473e869601d58087263b4e5fc7a71b5b1cbb
guillermobernal <43592420+guillermobernal@users.noreply.github.com>
2019-03-30 23:24:46 -0500

    last commit

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,6 @@
 var words = ["alpha","bravo","charlie","delta"];
 
-var guessesleft = 12;
+var guessesleft = 10;
 var lettersguessed = ""
 
 var guesseslefthtml = document.getElementById("guessesleft");
@@ -11,23 +11,42 @@ var losses = document.getElementById("losses");
 
 var lettersguessedhtml = document.getElementById("lettersguessed");
 
+var currentword = "";
+var currentwordhtml = document.getElementById("currentword");
+
+var currentwordBlank = "";
+
+
 
 
 function getword() {
     var guesseslefthtml = document.getElementById("guessesleft");
     guesseslefthtml.textContent = guessesleft;
     var randomNumber = getRandomNumber(0, words.length-1);
-    return words[randomNumber];
+    currentword = words[randomNumber];
+
+   
+    for (let index = 0; index < currentword.length; index++) {
+        currentwordBlank = currentwordBlank + "_" + " ";
+
+        
+    }
+    currentwordBlankhtml = document.getElementById("currentword"); 
+    
+    currentwordBlankhtml.textContent = currentwordBlank;
+    
+    return currentword;
+    
 }
+
 
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
   }
 
   document.onkeyup = function(event) {
-
       
-var guesseslefthtml = document.getElementById("guessesleft");
+    var guesseslefthtml = document.getElementById("guessesleft");
     var userGuess = event.key;
     guessesleft = guessesleft - 1;
     guesseslefthtml.textContent = guessesleft;
@@ -36,16 +55,29 @@ var guesseslefthtml = document.getElementById("guessesleft");
     var lettersguessedhtml = document.getElementById("lettersguessed");
     
 
-lettersguessed = lettersguessed + userGuess;
-lettersguessedhtml.textContent = lettersguessed;
+    lettersguessed = lettersguessed + userGuess;
+    lettersguessedhtml.textContent = lettersguessed;
 
+     if (currentword.indexOf(userGuess) >= 0)
+     {
+        var letterIndex = currentword.indexOf(userGuess);
+        currentwordBlank[letterIndex] = userGuess;
+        var temp = currentwordBlank.split("");
+        temp[letterIndex] = userGuess;
+        currentwordBlank = temp.join("");
+        //currentwordBlank = currentwordBlank.substr(0, letterIndex) + userGuess + currentwordBlank.substr(letterIndex);
+        
+        
+        currentwordBlankhtml = document.getElementById("currentword"); 
+        
+        currentwordBlankhtml.textContent = currentwordBlank;
 
-      alert("User guess: " + userGuess);
-      alert("Computer guess: " + computerGuess);
-
-      for (var i = 0; i < words.length; i++)
-
-
-    };
+     }
+     else
+     {
+         //letter not in word
+     }
+    
+   };
 
   
