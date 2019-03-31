0e53772f6b89ac7ee5a7ddaee83ca07b2a62517b
guillermobernal <43592420+guillermobernal@users.noreply.github.com>
2019-03-30 19:57:12 -0500

    fixed error

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -1,6 +1,6 @@
 var words = ["alpha","bravo","charlie","delta"];
 function getword() {
-    var rondomNumber = getRandomNumber(0, words.length-1);
+    var randomNumber = getRandomNumber(0, words.length-1);
     return words[randomNumber];
 }
 function getRandomNumber(min, max) {
